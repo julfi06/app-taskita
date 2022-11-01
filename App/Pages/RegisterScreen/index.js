6ab89309af212b7433955fd6a7d5eb3aa6ab5850
julfi06 <julfi2220@gmail.com>
2022-11-02 06:21:09 +0700

    first commit

---
+++ App/Pages/RegisterScreen/index.js
@@ -0,0 +1,140 @@
+import *  as React from 'react';
+import axios from 'axios'
+import {
+    SafeAreaView,
+    View,
+    Text,
+    StatusBar,
+    TextInput,
+    StyleSheet,
+    TouchableOpacity
+} from 'react-native'
+import { PrimaryButton } from '../../Componets'
+
+export default RegisterScreen = ({ navigation }) => {
+    const [name, onChangeName] = React.useState(null);
+    const [email, onChangeEmail] = React.useState(null);
+    const [password, onChangePassword] = React.useState(null);
+    const onRegister=()=>{
+        axios.post('https://data.mongodb-api.com/app/data-yvczw/endpoint/data/v1/action/insertOne',{
+            "dataSource": "Cluster0",
+            "database": "app_taskita",
+            "collection": "member",
+            "document": {
+              "name": name,
+              "email": email,
+              "password":password
+        }
+    },{
+        headers:{
+            'api-key': 'zYwAQaYVJ2hdF6WVlhy4gFM7i6IOGAcAJ5lips8IYEjIkXjoksjPpuTBZvGjt4uC'
+        }
+    }).then(res=>{
+        navigation.navigate('RegisterSuccessScreen')
+    }).catch(err=>{
+        console.log(err)
+        navigation.navigate('RegisterErrorScreen')
+    })
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar
+                barStyle="light-content"
+                animated={true}
+                backgroundColor="#131B63" />
+
+            <Text style={styles.headerText}>Register</Text>
+            <Text style={styles.BodyText}>Please input your valid data.</Text>
+
+            <TextInput style={[styles.inputStyle, { marginTop: 80 }]}
+                placeholder="Full Name"
+                onChangeText={onChangeName}
+                value={name} />
+            <TextInput style={[styles.inputStyle]}
+                placeholder="Email"
+                onChangeText={onChangeEmail}
+                value={email} />
+            <TextInput style={[styles.inputStyle]}
+                placeholder="Password"
+                secureTextEntry={true}
+                onChangeText={onChangePassword}
+                value={password} />
+
+            <PrimaryButton
+                customeStyle={styles.btnRegisterStyle}
+                title="REGISTER"
+                onPress={() => onRegister()} />
+
+            <View style={styles.footherText}>
+                <Text style={styles.smallFootherText}>Already have an account?</Text>
+                <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
+                    <Text style={styles.smallFootherTextRight}>Login here!</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.footherText2}>
+                <Text style={styles.smallFootherText}>By signing up, you are agree with our </Text>
+                <Text style={styles.smallFootherTextRight}>Terms & Conditions</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 15,
+        alignContent: 'center',
+        backgroundColor: '#131B63'
+    },
+    headerText: {
+        marginTop: 10,
+        fontSize: 73,
+        fontFamily: 'Roboto-Light',
+        color: 'white'
+    },
+    BodyText: {
+        marginTop: 10,
+        fontSize: 18,
+        fontFamily: 'Roboto-Light',
+        color: 'white'
+    },
+    inputStyle: {
+        height: 50,
+        marginTop: 15,
+        borderWidth: 1,
+        paddingLeft: 30,
+        borderRadius: 50,
+        borderColor: '#695D91',
+        fontSize: 16,
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor: '#695D91'
+    },
+    btnRegisterStyle: {
+        marginTop: 40
+    },
+    footherText: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 35
+    },
+    smallFootherText: {
+        fontFamily: 'Roboto-Light',
+        fontSize: 10,
+        color: 'white',
+    },
+    smallFootherTextRight: {
+        fontFamily: 'Roboto-Light',
+        fontSize: 10,
+        color: 'white',
+        marginLeft: 5,
+        textDecorationLine: 'underline'
+    },
+    footherText2: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 3
+    }
+})
